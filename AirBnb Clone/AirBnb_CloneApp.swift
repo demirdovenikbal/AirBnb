1f292d53c7687ebcdd09f63d71f58555895b44aa
demirdovenikbal <ikbaldemirdoven@icloud.com>
2023-12-23 14:14:53 -0800

    Initial Commit

---
+++ AirBnb Clone/AirBnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnb_CloneApp.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2023-12-23.
+//
+
+import SwiftUI
+
+@main
+struct AirBnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
