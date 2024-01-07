33704c791b472cac2a813cfe7ac15a7527e3dd3e
demirdovenikbal <ikbaldemirdoven@icloud.com>
2024-01-06 18:38:54 -0800

    initial setup, explore view, uploading assets etc...

---
+++ AirBnb Clone/Core/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-06.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ AirBnb Clone/Core/Listings/ListingItem.swift
@@ -0,0 +1,18 @@
+//
+//  ListingView.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-06.
+//
+
+import SwiftUI
+
+struct ListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingView()
+}
