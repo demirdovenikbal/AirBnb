78b0fe69ff91460828cdc2e057a89dd59745c3b0
demirdovenikbal <ikbaldemirdoven@icloud.com>
2024-01-09 00:31:20 -0800

    created seperate view modules from listingitemview such as listingdetailview and listingimagecarouselview

---
+++ AirBnb Clone/Core/Components/ListingImageCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingImageCarouselView.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-09.
+//
+
+import SwiftUI
+
+struct ListingImageCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingImageCarouselView()
+}
---
+++ AirBnb Clone/Core/Listings/ListingDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingDetailView.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-09.
+//
+
+import SwiftUI
+
+struct ListingDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
