d016e994ffae73b228250d671d6015f7b789a80f
demirdovenikbal <ikbaldemirdoven@icloud.com>
2024-01-10 23:49:32 -0800

    completed the listingdetailview

--- AirBnb Clone/Core/Listings/ListingDetailView.swift
+++ AirBnb Clone/Core/Listings/ListingDetailView.swift
@@ -6,13 +6,155 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ListingDetailView: View {
     var images = ["listing0", "listing1", "listing2", "listing3", "listing4"]
     var body: some View {
         ScrollView {
+            //listing image
             ListingImageCarouselView()
                 .frame(height: 320)
+            //details
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Miami Villa")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                VStack(alignment: .leading) {
+                    HStack(spacing: 2) {
+                        Image(systemName: "star.fill")
+                        Text("4.86")
+                        Text("-")
+                        Text("28 reviews")
+                            .underline()
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundStyle(.black)
+                    Text("Miami, Florida")
+                }
+                .font(.caption)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading)
+            Divider()
+            //host info view
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Entire villa hosted by John Smith")
+                        .font(.headline)
+                        .frame(width: 250, alignment: .leading)
+                    Text("4 guests - 4 bedrooms - 4 beds - 3 baths")
+                        .font(.caption)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                Image("ikbal")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+                    .scaledToFill()
+                    .clipShape(Circle())
+                    .padding(.trailing, 10)
+            }
+            .padding(.leading)
+            .padding(.top)
+            Divider()
+            //listing features
+            VStack(alignment: .leading, spacing: 1) {
+                ForEach(0..<2) {
+                    feature in
+                    HStack(spacing: 12) {
+                        Image(systemName: "medal")
+                        VStack(alignment: .leading) {
+                            Text("Superhost")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                            Text("Superhosts are experienced, highly rated hosts who are committed to providing great stars for guests.")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                        Spacer()
+                    }
+                }
+//                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            }
+            Divider()
+            //bedrooms
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Where you'll sleep")
+                    .font(.headline)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(1..<5) {
+                            bedroom in
+                            VStack {
+                                Image(systemName: "bed.double")
+                                Text("Bedroom \(bedroom)")
+                            }
+                            .frame(width: 132, height: 100)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.gray)
+                            }
+                        }
+                    }
+                }
+                .scrollTargetBehavior(.paging)
+            }
+            .padding()
+            Divider()
+            //listing amenities
+            VStack(alignment: .leading, spacing: 16) {
+                Text("What this place offers")
+                    .font(.headline)
+                ForEach(0..<5) {
+                    feature in
+                    HStack {
+                        Image(systemName: "wifi")
+                            .frame(width: 32)
+                        Text("Wifi")
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+            Divider()
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Where you'll be")
+                    .font(.headline)
+                Map()
+                    .frame(height: 200)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+            .padding()
+        }
+        .padding(.bottom, 64)
+        .overlay(alignment: .bottom) {
+            VStack {
+                Divider()
+                    .padding(.bottom)
+                HStack {
+                    VStack(alignment: .leading) {
+                        
+                    }
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Text("Reserve")
+                            .foregroundStyle(.white)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 140, height: 40)
+                            .background(.pink)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    }
+                }
+                .padding(.horizontal, 32)
+                .padding(.bottom, 30)
+                .padding(.top, 5)
+            }
+            .background(.white)
         }
         .ignoresSafeArea()
     }
