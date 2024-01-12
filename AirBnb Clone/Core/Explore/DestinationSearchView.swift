2f3517310615a49f38dd11837b271d903b9d1e26
demirdovenikbal <ikbaldemirdoven@icloud.com>
2024-01-11 22:49:24 -0800

    implemented destinationsearchview

---
+++ AirBnb Clone/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearchView.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-11.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
