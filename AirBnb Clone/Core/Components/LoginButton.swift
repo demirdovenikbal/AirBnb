838f20475ec3175573346bfdb6b7da8be6851f8e
demirdovenikbal <ikbaldemirdoven@icloud.com>
2024-01-13 16:27:23 -0800

    implemented the demo of WishlistView

---
+++ AirBnb Clone/Core/Components/LoginButton.swift
@@ -0,0 +1,18 @@
+//
+//  LoginButto .swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-13.
+//
+
+import SwiftUI
+
+struct LoginButto_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginButto_()
+}
---
+++ AirBnb Clone/Core/WishList/WishlistView.swift
@@ -0,0 +1,18 @@
+//
+//  WishlistView.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-13.
+//
+
+import SwiftUI
+
+struct WishlistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WishlistView()
+}
