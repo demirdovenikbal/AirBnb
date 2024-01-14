16b0071761bc57ca5cc5a2b1f03f42c7aef93971
demirdovenikbal <ikbaldemirdoven@icloud.com>
2024-01-13 16:10:32 -0800

    implemented the demo of profile view UI

---
+++ AirBnb Clone/Core/Profile/ProfileOptionRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileOptionRowView.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-13.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileOptionRowView()
+}
