f4d64419c193e687e4e88f1142d84617e9a06bbc
demirdovenikbal <ikbaldemirdoven@icloud.com>
2024-01-22 23:06:30 -0800

    implemented listingmapview navigation

---
+++ AirBnb Clone/Core/Listings/View/ListingMapView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingMapView.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-21.
+//
+
+import SwiftUI
+
+struct ListingMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingMapView()
+}
