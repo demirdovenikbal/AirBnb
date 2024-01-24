5985fc470e2db5ffbaf5eca80e047851a93690f6
demirdovenikbal <ikbaldemirdoven@icloud.com>
2024-01-23 23:20:08 -0800

    finished the implementation of the demo of the MapView with mock data

---
+++ AirBnb Clone/Core/Listings/View/MapListingPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  MapListingPreviewView.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-23.
+//
+
+import SwiftUI
+
+struct MapListingPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapListingPreviewView()
+}
