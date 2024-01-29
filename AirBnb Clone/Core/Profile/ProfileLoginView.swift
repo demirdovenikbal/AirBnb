2f3756dabe5a3b0403b99b6ac276fa475dfa03b8
demirdovenikbal <ikbaldemirdoven@icloud.com>
2024-01-29 12:52:31 -0800

    implemented profileview with a mock account

---
+++ AirBnb Clone/Core/Profile/ProfileLoginView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileLoginView.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-29.
+//
+
+import SwiftUI
+
+struct ProfileLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileLoginView()
+}
---
+++ AirBnb Clone/Core/Profile/UserProfileHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileHeaderView.swift
+//  AirBnb Clone
+//
+//  Created by Ikbal Demirdoven on 2024-01-29.
+//
+
+import SwiftUI
+
+struct UserProfileHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileHeaderView()
+}
